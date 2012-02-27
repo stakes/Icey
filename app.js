88e813ccef1ec753867c72573597b382937101e7
Jay Stakelon <stakelon@gmail.com>
2012-02-26 18:11:44 -0800

    create a user object with mongoose-auth

--- app.js
+++ app.js
@@ -1,48 +1,84 @@
-
-/**
- * Module dependencies.
- */
+// Let's get it
 
 var env = process.env.NODE_ENV || 'development';
 var port = process.env.PORT || 3000;
 
-
 var express = require('express')
   , routes = require('./routes')
-  , everyauth = require('everyauth')
   , config_file = require('yaml-config')
   , config = config_file.readConfig('config/config.yaml', env)
+  , mongoose = require('mongoose')
+  , mongooseauth = require('mongoose-auth')
   , github = require('./lib/github');
 var app = module.exports = express.createServer();
+var Schema = mongoose.Schema;
+var ObjectId = mongoose.SchemaTypes.ObjectId;
+
+
+
+// Schemas/models
+var UserSchema = new Schema({});
+var User;
+
+UserSchema.plugin(mongooseauth, {
+  everymodule: {
+    everyauth: {
+        User: function () {
+          return User;
+        }
+    }
+  },
+  github: {
+    everyauth: {
+        myHostname: config.github.hostUri
+      , appId: config.github.appId
+      , appSecret: config.github.appSecret
+      , redirectPath: '/'
+    }
+  }
+})
+
+mongoose.model('User', UserSchema);
+mongoose.connect(config.db.uri);
+User = mongoose.model('User');
+
+
+
+
+// Mongoose-auth
+mongooseauth.helpExpress(app);
+
 
 
 
 // Everyauth
-everyauth.helpExpress(app);
-everyauth.debug = true;
-everyauth.github
-  .appId(config.github.appId)
-  .appSecret(config.github.appSecret)
-  .scope('user,repo')
-  .findOrCreateUser( function (session, accessToken, accessTokenExtra, ghUser) {
-      session.uid = ghUser.id
-      session.oauth = accessToken
-      return ghUser.id
-  })
-  .redirectPath('/');
-  
-everyauth.everymodule.handleLogout(function(req, res) {
-  req.logout();
-  req.session.uid = null;
-  res.writeHead(303, { 'Location': this.logoutRedirectPath() });
-  res.end();
-});
-everyauth.everymodule.logoutPath('/logout');
-everyauth.everymodule.logoutRedirectPath('/');
+// everyauth.helpExpress(app);
+// everyauth.debug = true;
+// everyauth.github
+//   .appId(config.github.appId)
+//   .appSecret(config.github.appSecret)
+//   .scope('user,repo')
+//   .findOrCreateUser( function (session, accessToken, accessTokenExtra, ghUser) {
+//       session.uid = ghUser.id
+//       session.oauth = accessToken
+//       return ghUser.id
+//   })
+//   .redirectPath('/');
+//   
+// everyauth.everymodule.handleLogout(function(req, res) {
+//   req.logout();
+//   req.session.uid = null;
+//   res.writeHead(303, { 'Location': this.logoutRedirectPath() });
+//   res.end();
+// });
+// everyauth.everymodule.logoutPath('/logout');
+// everyauth.everymodule.logoutRedirectPath('/');
+
 
 
 
 // Configuration
+
 var pub = __dirname + '/public';
 app.configure(function(){
   app.set('views', __dirname + '/views');
@@ -51,8 +87,8 @@ app.configure(function(){
   app.use(express.methodOverride());
   app.use(express.cookieParser());
   app.use(express.session({secret: 'stakes'}));
-  app.use(app.router);
-  app.use(everyauth.middleware());
+  // app.use(app.router);
+  app.use(mongooseauth.middleware());
   app.use(express.static(pub));
 });
 
@@ -65,6 +101,8 @@ app.configure('production', function(){
 });
 
 
+
+
 // Routes
 
 app.get('/', routes.index);
@@ -74,6 +112,9 @@ app.get('/projects', routes.getProjects);
 app.get('/project/:user/:id/:key', routes.getSingleProject);
 app.get('/issue/update/:user/:repo/:issue/:label/:key', routes.updateIssue);
 app.get('/issue/close/:user/:repo/:issue/:key', routes.closeIssue);
+
+
+
 
 // Server
 
